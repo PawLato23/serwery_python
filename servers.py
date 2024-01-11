19a8c70e7cf31ef26b08f797a596942d61cb83b4
Adam-Lek <129278265+Adam-Lek@users.noreply.github.com>
2024-01-11 23:55:18 +0100

    klasa Product włącznie z testami TO DO: UML
    Nie dodałem do UML jeszcze.

--- servers.py
+++ servers.py
@@ -5,15 +5,38 @@ from typing import Optional
 
 
 class Product:
-
+    def __init__(self, name: str, price: float):
+        if not name.isalnum():  #sprawdzenie czy na pewno tylko litery i cyfry
+            raise ValueError
+        if not isinstance(price, (int, float)):
+            raise ValueError
+        #sprawdzenie, czy na pewno foramat xxx000
+        nnum = nchr = 0
+        for it in name:
+            if it.isdigit():
+                nnum += 1
+            elif nnum == 0: #sprawdza czy na pewno najpierw sa literki, potem cyfry
+                nchr += 1
+            else:
+                raise ValueError     
+        if nnum == 0 or nchr == 0:  #sprawdza czy napewno jest przynajmniej jedna cyfra i literka
+            raise ValueError
+        self.name = name
+        self.price = price
+    
     def __eq__(self, other):
-        return None
+        if hash(self) == hash(other):
+            return True
+        return False
 
     def __hash__(self):
         return hash((self.name, self.price))
+    
+    def __str__(self):
+        return f"{self.name} : {self.price}"
 
 
-class TooManyProductsFoundError:
+class TooManyProductsFoundError(Exception):
     pass
 
 
@@ -29,3 +52,7 @@ class Client:
 
     def get_total_price(self, n_letters: Optional[int]) -> Optional[float]:
         raise NotImplementedError()
+        
+p1 = Product("aaa1321", 12)
+p2 = Product("aab2", 13)
+p3 = Product("aaa1321", 12)
--- servers__tests.py
+++ servers__tests.py
@@ -1,9 +1,81 @@
 import unittest
+from servers import Product
 
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+        #Class Product:
+
+            #__init__
+        # isalnum()
+        with self.assertRaises(ValueError):
+            Product("", 2137)
+        with self.assertRaises(ValueError):
+            Product("a 1", 12)
+        with self.assertRaises(ValueError):
+            Product("!a1", 12)
+        with self.assertRaises(ValueError):
+            Product("a1%", 12)
+        # price to float lub int
+        Product("a1", int(123))
+        Product("a1", float(123.123))
+        with self.assertRaises(ValueError):
+            Product("a1", "a")
+        # przynajmniej 1 cyfra i 1 litera
+        with self.assertRaises(ValueError):
+            Product("a", 12)
+        with self.assertRaises(ValueError):
+            Product("ba", 2137)
+        with self.assertRaises(ValueError):
+            Product("1", 123131314124)
+        with self.assertRaises(ValueError):
+            Product("12", 2137)
+        # format xxx000
+        Product("xxx000", 2137)        
+        with self.assertRaises(ValueError):
+            Product("1a", 2137)
+        with self.assertRaises(ValueError):
+            Product("a1a", 2137)
+        with self.assertRaises(ValueError):
+            Product("aaaa1234asdf", 2137)
+        with self.assertRaises(ValueError):
+            Product("1123aasd2141", 2137)
+            
+            #__hash__ test unikatowosci
+        self.assertNotEqual(hash(Product("a1", 1)), hash(Product("A1", 1)))
+        
+        p1 = Product("aaa2137", 12)
+        p2 = Product("aab2", 13)
+        p3 = Product("aaa2137", 12)
+        p4 = Product("aaa2137", 100)
+        p5 = Product("Fus123", 100)
+        p6 = Product("Fus123", 100)
+        
+        self.assertEqual(hash(p1), hash(p1))
+        self.assertEqual(hash(p2), hash(p2))
+        self.assertEqual(hash(p1), hash(p3))
+        self.assertEqual(hash(p5), hash(p6))
+        self.assertNotEqual(hash(p1), hash(p2))
+        self.assertNotEqual(hash(p1), hash(p4))
+        self.assertNotEqual(hash(p1), hash(p5))
+        self.assertNotEqual(hash(p3), hash(p4))
+        self.assertNotEqual(hash(p4), hash(p5))
+            
+            #__eq__
+        self.assertNotEqual(Product("a1", 1), Product("A1", 1))
+        
+        self.assertEqual(p1, p3)
+        self.assertEqual(p5, p6)
+        self.assertEqual(p6, p6)
+        self.assertEqual(p5, p5)
+        self.assertNotEqual(p1, p2)
+        self.assertNotEqual(p1, p4)
+        self.assertNotEqual(p1, p5)
+        self.assertNotEqual(p1, p6)
+        self.assertNotEqual(p2, p1)
+        self.assertNotEqual(p2, p3)
+        self.assertNotEqual(p2, p4)
+        self.assertNotEqual(p2, p5)
 
 
 if __name__ == '__main__':
